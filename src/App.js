1100e89e4b6240d297424afa0ff88b623d35b211
Rhubert710 <73670513+Rhubert710@users.noreply.github.com>
2022-09-20 10:14:08 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import SearchBar from './components/SearchBar';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Drink Companion</h1>
+
+      <SearchBar/>
+
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SearchBar() {
+  return (
+    <input type="text" style={{width:'80vw', 
+                                maxWidth:'44rem',
+                                height:'1.9rem',
+                                borderRadius:'10px'}}/>
+  )
+}
+
+export default SearchBar
